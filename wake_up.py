f6089d6c531c5442cd9ce54bc597898343da84ee
NauticaWeb <el_druida@hotmail.com>
2025-06-01 09:35:27 +0200

    Update wake_up.py

--- wake_up.py
+++ wake_up.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+import time
 
 urls = [
     "https://calculadorasnauticas.streamlit.app/",
@@ -8,12 +9,16 @@ urls = [
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     context = browser.new_context()
-    page = context.new_page()
+    
     for url in urls:
         try:
             print(f"Abriendo {url}")
+            page = context.new_page()
             page.goto(url, timeout=30000)
+            time.sleep(5)  # Simula permanencia real en la página
             print(f"{url} cargada correctamente")
+            page.close()
         except Exception as e:
             print(f"Error accediendo a {url}: {e}")
+    
     browser.close()
